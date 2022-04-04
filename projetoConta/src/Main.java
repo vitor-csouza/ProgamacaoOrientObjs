d833d15638f2fc98b0b04280ab1248f5a6cdb870
vitorcsouza <64599506+vitor-csouza@users.noreply.github.com>
2022-04-04 09:40:51 -0300

    Update Main.java

--- projetoConta/src/Main.java
+++ projetoConta/src/Main.java
@@ -12,9 +12,9 @@ public class Main {
 		//Cliente cli3 = new Cliente("Pedro", "57533");
 		
 
-		contas[0] = new Conta(new Cliente("Vitor", "12234"), 2450);
-		contas[1] = new Conta(new Cliente("Davi", "124535"), 1730);
-		contas[2] = new Conta(new Cliente("Pedro", "57533"), 4680);
+		contas[0] = new Conta(new Cliente( "12234", "Vitor"), 2450);
+		contas[1] = new Conta(new Cliente("124535", "Davi"), 1730);
+		contas[2] = new Conta(new Cliente("57533", "Pedro"), 4680);
 
 		System.out.println(contas[0].getDados());
 		System.out.println(contas[1].getDados());
