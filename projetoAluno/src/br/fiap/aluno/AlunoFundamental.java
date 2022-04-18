c015ad2137b5d44f86b987c314f093e98b6ed62c
vitorcsouza <64599506+vitor-csouza@users.noreply.github.com>
2022-04-18 12:03:26 -0300

    Add files via upload

--- projetoAluno/src/br/fiap/aluno/AlunoFundamental.java
+++ projetoAluno/src/br/fiap/aluno/AlunoFundamental.java
@@ -5,6 +5,11 @@ public class AlunoFundamental extends Aluno {
 	//atributos específicos da classe
 	private int serie;
 
+	public AlunoFundamental(long rm, String nome, double prova1, double prova2, int serie) {
+		super(rm, nome, prova1, prova2);
+		this.serie = serie;
+	}
+
 	public double calcularMedia() {
 		return (prova1+prova2)/2;
 	}
